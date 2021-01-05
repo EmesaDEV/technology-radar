13bc002f5f92c0fb95623979d864a0e4e37ae07d
Radu Voinea <radu.voinea@emesa.nl>
2021-01-05 13:58:48 +0100

    changes: fixed err msg

--- src/util/exceptionMessages.js
+++ src/util/exceptionMessages.js
@@ -1,6 +1,6 @@
 const ExceptionMessages = {
   TOO_MANY_QUADRANTS: 'There are more than 4 quadrant names listed in your data. Check the quadrant column for errors.',
-  TOO_MANY_RINGS: 'More than 4 rings.',
+  TOO_MANY_RINGS: 'More than 6 rings.',
   MISSING_HEADERS: 'Document is missing one or more required headers or they are misspelled. ' +
   'Check that your document contains headers for "name", "ring", "quadrant", "isNew", "description".',
   MISSING_CONTENT: 'Document is missing content.',
